766d4c2eda8d9cf9c2899aa825e90ac01d42b047
Khunaiko <pasuaiko21089@gmail.com>
2023-12-12 11:13:48 +0700

    first commit

---
+++ src/components/ComponentA.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'ComponentA',
+        data() {
+            return {
+                name: "ComponentA",
+            }
+        },
+    }
+</script>
+
---
+++ src/components/ComponentB.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'ComponentB' ,
+       data: ()=> {
+        return {
+            name: "ComponentB",
+          }
+       },
+    }
+</script>
+
